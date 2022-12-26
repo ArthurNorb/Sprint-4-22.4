3c7182e46b965470e64ab0c115467fe6f709b19d
ArthurNorb <arthunorberto4@gmail.com>
2022-12-26 15:18:12 -0300

    adicinado InicialPage.java (moldes do PageObject)

---
+++ frontend/maven/src/test/java/br/com/terralab/ps/sig/test/InicialPage.java
@@ -0,0 +1,33 @@
+package br.com.terralab.ps.sig.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class InicialPage {
+	
+	
+	private static final String URL_INICIAL = "https://inteligenciageografica-a377d.web.app/";
+	private WebDriver browser;
+	
+	public InicialPage() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		this.browser = new ChromeDriver();
+        browser.navigate().to(URL_INICIAL);
+	}
+	
+	public void fechar() {
+		this.browser.quit();
+	}
+
+	public void preencheBuscaDeCidade(String cidade) {
+        this.browser.findElement(By.id("input-field-search")).sendKeys(cidade);		
+	}
+
+	public void clicaBotaoPesquisar() {
+		WebElement botaoPesquisar = this.browser.findElement(By.id("button-search-input-field-search"));
+        new Actions(this.browser).click(botaoPesquisar).perform();		
+	}
+}
--- frontend/maven/src/test/java/br/com/terralab/ps/sig/test/testSig.java
+++ frontend/maven/src/test/java/br/com/terralab/ps/sig/test/testSig.java
@@ -1,40 +1,27 @@
 package br.com.terralab.ps.sig.test;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
 public class testSig {
 	
-	private static final String URL_INICIAL = "https://inteligenciageografica-a377d.web.app/";
-	private WebDriver browser;
-	
-	@BeforeAll
-	public static void beforeAll() {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-	}
+	private InicialPage paginaDeInicio;
 	
 	@BeforeEach
 	public void beforeEach() {
-		this.browser = new ChromeDriver();
-        browser.navigate().to(URL_INICIAL);
+		this.paginaDeInicio = new InicialPage();
 	}
 	
-	/*@AfterEach
+	@AfterEach
 	public void afterAll() {
-		this.browser.quit();
-	}*/
+		paginaDeInicio.fechar();
+	}
 	
 	@Test
 	public void deveria_AcharPontoNoMapa_SemBuscaAutomatica() {
-        this.browser.findElement(By.id("input-field-search")).sendKeys("ipatinga");
-        WebElement botaoPesquisar = this.browser.findElement(By.id("button-search-input-field-search"));
-        new Actions(this.browser).click(botaoPesquisar).perform();
+		paginaDeInicio.preencheBuscaDeCidade("Ipatinga");
+		paginaDeInicio.clicaBotaoPesquisar();
 	}
 
 }
